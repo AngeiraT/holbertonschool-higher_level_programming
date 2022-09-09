73b263249c81ab49507bad0e27a69a885841ed89
AngeiraT <thaispur04@icloud.com>
2022-09-09 10:52:57 -0400

    indent

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(100):
     if number < 99:
-     print(" {0:0=2d},".format(number),end="")
+        print(" {0:0=2d},".format(number),end="")
     else:
         print(" {}".format(number),end="")
 
