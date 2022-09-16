12b3effffc3caef410e0d1c3911fb56abf66bda2
AngeiraT <thaispur04@icloud.com>
2022-09-16 16:49:08 -0400

    change name

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,5 @@ from termios import IEXTEN
 def square_matrix_simple(matrix=[]):
     new_matrix= []
     for row in matrix:
-        new_matrix.append([value**2 for value in row])
+        new_matrix.append([val**2 for val in row])
     return new_matrix
