a055beb52cb1db958da1ff7d39634fec78beb0ec
AngeiraT <thaispur04@icloud.com>
2022-09-16 14:10:13 -0400

    she bang

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     # Assume first number in list is largest
     # initially and assign it to variable "max"
     max_value = my_list[0] 
-    for x in my_list:
+    # Now traverse through the list and compare
+    for x in my_list: 
     # each number with "max" value. Whichever is
     # largest assign that value to "max'.
         if x > max_value:
