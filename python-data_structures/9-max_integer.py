2043c43ae434ec5d9c11be1fbb66105ec64dcac7
AngeiraT <thaispur04@icloud.com>
2022-09-16 14:08:50 -0400

    added comments

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    # Assume first number in list is largest
+    # initially and assign it to variable "max"
+    max_value = my_list[0] 
+    for x in my_list:
+    # each number with "max" value. Whichever is
+    # largest assign that value to "max'.
+        if x > max_value:
+            max_value = x
+    return max_value
