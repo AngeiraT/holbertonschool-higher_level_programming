db5969a32ace67c214c15e15f37f4591cfd90bd4
AngeiraT <thaispur04@icloud.com>
2022-09-16 16:50:06 -0400

    erase typo

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-from termios import IEXTEN
-
-
 def square_matrix_simple(matrix=[]):
     new_matrix= []
     for row in matrix:
-        new_matrix.append([val**2 for val in row])
+        new_matrix.append([value**2 for value in row])
     return new_matrix
