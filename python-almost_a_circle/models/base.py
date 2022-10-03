c6ba315609183483b023cb314eb1f13556ee6a18
AngeiraT <thaispur04@icloud.com>
2022-10-03 19:23:35 -0400

    added main

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Base"""
+
+from ast import arg
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            id = arg
+        else:
+            __nb_objects =+ 1 :
+            id = 
