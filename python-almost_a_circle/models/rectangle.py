11d835a3817f88a59b711b7d7306e5bfab63f360
AngeiraT <thaispur04@icloud.com>
2022-10-03 21:10:42 -0400

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""
+Module for the class Rectangle
+Task 2: Write the class Rectangle that inherits from Base
+"""
+
+
+from re import X
+from models.base import Base
+class Rectangle(Base):
+    """
+    Class Rectangle
+    width, height, x, y = private instance attribute
+
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor Rectangle"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Width property getter
+    
+        Returns: private instance attribute
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be > 0")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.value = value
+
+    @property
+    def height(self):
+        """Height property getter
+
+        Return: private instance attribute
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """X property getter
+
+        Return: private instance attribute
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """X setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Y property getter
+        
+        Return: private isntance attribute
+        """
+        return self.__y
+
+    @y.getter
+    def y(self, value):
+        """Y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
