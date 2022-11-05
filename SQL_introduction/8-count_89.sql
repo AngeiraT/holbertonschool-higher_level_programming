56470e82e5bcdb60c425242ca2c3a0a9c534f5c1
AngeiraT <thaispur04@icloud.com>
2022-11-05 11:02:06 -0400

    EDITED COMMENTS TASK 8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
--- Creates a database if not exist
--- Create a database
+-- script that displays the number of records 
+-- with id = 89 in the table first_table of the database hbtn_0c_0
 SELECT COUNT(*) FROM first_table WHERE id = 89;
