26224f4b4da307428cf19a0fe5748bddb756d575
AngeiraT <thaispur04@icloud.com>
2022-11-07 19:01:17 -0400

    c.states

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,4 +4,5 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
+SELECT c.id, c.name FROM cities AS c, states AS s
+    WHERE c.state_id=s.id AND s.name = "California" ORDER BY c.id ASC;
