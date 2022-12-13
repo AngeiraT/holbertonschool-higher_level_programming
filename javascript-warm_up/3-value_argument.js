b10868466f199d4b7a7a6eec3c320aad4e8c4d3c
AngeiraT <thaispur04@icloud.com>
2022-12-12 22:30:53 -0400

    edited if statemtent

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-
-if (process.argv[2]) { // This is an array containing the cmd. line arguments
-    console.log(process.argv[2]);
-  } else {
-    console.log('No argument');
-  }
+console.log(process.argv[2] || 'No argument');
