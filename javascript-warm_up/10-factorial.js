dfc26e6bf00da430dd9732995e19d783e67232aa
AngeiraT <thaispur04@icloud.com>
2022-12-12 22:54:45 -0400

    changr into n!

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (isNaN(n) || n < 2) {
+    if (!n) { // Factorial of NaN
       return 1;
-    } else {
-      return n * factorial(n - 1);
     }
+    return n * factorial(n - 1);
   }
   
   console.log(factorial(process.argv[2]));
-  
