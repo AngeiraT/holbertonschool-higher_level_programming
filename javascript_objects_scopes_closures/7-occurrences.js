89cf859f45fa676eb694c86364f9a924abed6dde
AngeiraT <thaispur04@icloud.com>
2022-12-13 19:56:11 -0400

    added filter

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 exports.nbOccurences = function (list, searchElement) {
-    let occurrences = 0;
-  
-    for (const item of list) {
-      if (item !== searchElement) { continue; }
-      occurrences++;
-    }
-  
-    return occurrences;
+    const number = list.filter(item => item === searchElement).length;
+    return number;
   };
