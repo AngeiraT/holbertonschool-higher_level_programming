f82568443d672ed8611c96ec0a02f5b83a6b2d4e
AngeiraT <thaispur04@icloud.com>
2022-12-12 22:40:28 -0400

    added comments

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < line.length; i++) {
-  console.log(line[i]);
+
+/* Array.lenght => Gets or sets the length of the array.
+This is a number one higher than the highest index in the array.
+*/
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
