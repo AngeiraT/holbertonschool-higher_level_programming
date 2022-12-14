4b69a7498d459019b03241b5071afc52d4a1e1bb
AngeiraT <thaispur04@icloud.com>
2022-12-14 10:36:46 -0400

    fix semistandard

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let occurrences = 0;
-  
-    for (const item of list) {
-      if (item !== searchElement) { continue; }
-      occurrences++;
-    }
-  
-    return occurrences;
-  };
+  let occurrences = 0;
+
+  for (const item of list) {
+    if (item !== searchElement) { continue; }
+    occurrences++;
+  }
+
+  return occurrences;
+};
