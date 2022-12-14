38873ec40c246f5907aad81554991797c6f4c4a4
AngeiraT <thaispur04@icloud.com>
2022-12-13 20:01:03 -0400

    edited for loop

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-    const number = list.filter(item => item === searchElement).length;
-    return number;
+    let occurrences = 0;
+  
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === searchElement) {
+        occurrences++;
+      }
+    }
+    return occurrences;
   };
